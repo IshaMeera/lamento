d5d626041e5a1ec8a02b43176bc1a177bbfdbebd
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-09-28 19:53:14 +0530

    title added in Head

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,6 +11,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+            <title>Lamento</title>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;1,300&display=swap"
@@ -19,6 +21,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
         </body>
       </Html>
     );
