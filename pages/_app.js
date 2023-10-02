11597d31fe330947712dc01e1ea2fa3475df6fa1
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-10-02 09:30:49 +0530

    fix: :chart_with_upwards_trend: analytics script is beed moved

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
 import "../styles/tailwind.css";
 import "../styles/slick.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
+    </>
+  );
 }
 
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -21,7 +20,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
         </body>
       </Html>
     );
